791825d97049bc26fc2a1c5d9b94f3f590f4229e
gelm0 <gustav.ek@gmail.com>
2020-09-29 19:00:07 +0200

    Refactored into a huffman class, working on getting a bitarray dict so that we can encode properly, after that we need to construct headers and decode functions

--- huffman.py
+++ huffman.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 import heapq
 import sys
-from heapq import heapify
-from heapq import heappop
-
-codes_dict = {}
+from bitarray import bitarray
+from heapq import heapify,heappop
 
 class Node:
     def __init__(self, symbol, freq):
@@ -33,35 +31,96 @@ class Node:
     def get_lchild(self):
         return self._right_child
 
-def count_freq(txt):
-    freq = {}
-    for c in txt:
-        if c in freq:
-            freq[c] += 1
-        else:
-            freq[c] = 1
-    return freq
+class Huffman:
+    def __init__(self):
+        self.codes_dict = {}        
+        self.bitarray_dict = {}
 
-def make_codesdict(node, code):
-    if node == None:
-        return
+    def count_freq(self, txt):
+        freq = {}
+        for c in txt:
+            if c in freq:
+                freq[c] += 1
+            else:
+                freq[c] = 1
+        return freq
+
+    def make_codes_dict(self, node, code):
+        if node == None:
+            return
     
-    if node._symbol != None:
-        codes_dict[node._symbol] = code
-        return
+        if node._symbol != None:
+            self.codes_dict[node._symbol] = code
+            return
 
-    make_codesdict(node._left_child, code + "0")
-    make_codesdict(node._right_child, code + "1")
+        self.make_codesdict(node._left_child, code + "0")
+        self.make_codesdict(node._right_child, code + "1")
 
-def compress(txt):
-    return "".join([codes_dict[c] for c in txt])
+    def make_bitarray_codes_dict(self):
+        
 
+    def encode_file(self, file_name):
+        file_data = open(file_name, 'rb').read()
+        self.encode(file_data)
+
+    def heapify_tree(self, char_freq):
+        node_list = []
+        for k,v in sorted(char_freq.items(), key=lambda val : val[1]):
+            node_list.append(Node(k,v))
+        heapify(node_list)
+        return node_list
+
+    def build_huffman_tree(self, heap_list):
+        while len(heap_list) > 1:
+            lnode = heapq.heappop(heap_list)
+            rnode = heapq.heappop(heap_list)
+            pnode = Node(None, lnode._freq + rnode._freq)
+            pnode.set_childs(lnode, rnode)
+            heapq.heappush(heap_list, pnode) 
+        return heapq.heappop(heap_list)
+
+    def encode(self, file_data):
+        char_freq = self.count_freq(file_data)
+        heap_list = self.heapify_tree(char_freq) 
+        root = self.build_huffman_tree(heap_list)
+        self.make_codes_dict(root, "")
+        print(self.codes_dict)
+
+    def test_compress(self, txt):
+        encode = bitarray()
+        print(encode.encode(self.codes_dict, txt))
+
+
+    def compress(self, txt):
+        return "".join([self.codes_dict[c] for c in txt])
+
+# https://pypi.org/project/bitarray/
+# We need to construct a bitarray encoding dict.
 f = open('testfile', 'rb').read()
-freq = count_freq(f)
-node_list = []
-for k,v in sorted(freq.items(), key=lambda val : val[1]):
-    node_list.append(Node(k,v))
-heapify(node_list)
+huffman = Huffman()
+huffman.encode_file('testfile')
+output = huffman.compress(f)
+output_file = open('output_binary', 'wb')
+print(output)
+print(output.encode())
+huffman.test_compress(f)
+
+output_file.write(output.encode())
+#huffman = Huffman()
+#freq = huffman.count_freq(f)
+#node_list = []
+#for k,v in sorted(freq.items(), key=lambda val : val[1]):
+#    node_list.append(Node(k,v))
+#heapify(node_list)
+
+# Pop the last item, the root node and traverse
+#root = heapq.heappop(node_list)
+#make_codesdict(root, "")
+#comp = compress(f)
+#fcomp = open('compfile', 'wb')
+#fcomp.write(str.encode(comp))
+#print(sys.getsizeof(f), sys.getsizeof(comp))
+#print(node_list[0].get_freq())
 
 #for i in node_list:
 #    print(i._symbol, i._freq)
@@ -69,18 +128,3 @@ heapify(node_list)
 #for node in node_list:
 #    print(node.get_freq())
 
-while len(node_list) > 1:
-    lnode = heapq.heappop(node_list)
-    rnode = heapq.heappop(node_list)
-    pnode = Node(None, lnode._freq + rnode._freq)
-    pnode.set_childs(lnode, rnode)
-    heapq.heappush(node_list, pnode) 
-
-# Pop the last item, the root node and traverse
-root = heapq.heappop(node_list)
-make_codesdict(root, "")
-comp = compress(f)
-fcomp = open('compfile', 'wb')
-fcomp.write(str.encode(comp))
-print(sys.getsizeof(f), sys.getsizeof(comp))
-#print(node_list[0].get_freq())
